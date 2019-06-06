f77d3e8d4eea301d6bc11f41e3ec35cf59a436d8
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-06 13:55:34 -0400

    added scrolling bg/fg, player commented out

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,12 +9,16 @@ export default class GameScene extends Phaser.Scene {
 
     this.load.tilemapTiledJSON('tilemap', 'assets/tilemap.json');
     this.load.image('tileset', 'assets/tileset.png');
-    this.load.spritesheet('player', 'assets/Spritesheets/idle-sheet.png', { frameWidth: 32, frameHeight: 26});
-    this.load.image('bg', 'assets/Landscape.gif');
+    this.load.image('bg', 'assets/background.png');
+    this.load.image('fg', 'assets/foreground.png');
 
   }
 
   create () {
+
+    //Create Background
+    this.background = this.add.tileSprite(this.game.config.width/2, this.game.config.height/2, 1200, 640, 'bg');
+    this.foreground = this.add.tileSprite(this.game.config.width/2, this.game.config.height/2, 1200, 640, 'fg');
     
     //Create Map & Layer
     var map = this.make.tilemap({key: 'tilemap'});
@@ -27,7 +31,14 @@ export default class GameScene extends Phaser.Scene {
     this.physics.world.bounds.height = layer.height;
 
     //Instantiate Player
-    var player = this.add.sprite(16, 576, 'player');
+    //var player = this.add.sprite(16, 576, 'player');
 
+  }
+
+  update() {
+
+    //Scroll Background
+    this.background.tilePositionX += 2;
+    this.foreground.tilePositionX += 4;
   }
 };
